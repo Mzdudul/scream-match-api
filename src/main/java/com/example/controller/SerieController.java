d8b1f518040c532de94cb70c077c3e3988b37902
Mzdudul <duduzim.gon05@gmail.com>
2025-07-24 17:07:10 -0300

    implementando a busca pro id

--- src/main/java/com/example/controller/SerieController.java
+++ src/main/java/com/example/controller/SerieController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dto.SerieDTO;
@@ -11,17 +13,31 @@ import com.example.dto.SerieDTO;
 import com.example.screamatch_api.service.SerieService;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
     @Autowired
     private SerieService serieService;
 
-    @GetMapping("/series")
-    public List<SerieDTO> obterSeries(SerieService serieService) {
+    @GetMapping()
+    public List<SerieDTO> obterSeries() {
         return serieService.obterTodasAsSeries();
         
     }
 
+    @GetMapping("/top5")
+    public List<SerieDTO> obterTop5Series() {
+        return serieService.obterTop5Series();
+    }
 
+    @GetMapping("/lancamentos")
+    public List<SerieDTO> obterTop5Lancamentos() {
+        return serieService.obterTop5Lancamentos();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id) {
+        return serieService.obterPorId(id);
+    }
     
     
 
--- src/main/java/com/example/screamatch_api/service/SerieService.java
+++ src/main/java/com/example/screamatch_api/service/SerieService.java
@@ -1,21 +1,49 @@
 package com.example.screamatch_api.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.dto.SerieDTO;
+import com.example.screamatch_api.model.Serie;
 import com.example.screamatch_api.repository.SerieRepository;
 
 @Service
 public class SerieService {
     
-    private SerieRepository serieRepository;
+    @Autowired
+    private SerieRepository repositorio;
 
     public List<SerieDTO> obterTodasAsSeries(){
-        return serieRepository.findAll()
-                .stream()
+        return converteDados(repositorio.findAll());                
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+         return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
+    }
+
+    public List<SerieDTO> obterTop5Lancamentos() {
+        return converteDados(repositorio.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+    }
+
+    public SerieDTO obterPorId(Long id) {
+       Optional <Serie> serie = repositorio.findById(id);
+
+       if(serie.isPresent()){
+        Serie s = serie.get();
+        return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(),
+                        s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+       }
+       return null;
+        
+                
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series) {
+        return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(),
                         s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());
